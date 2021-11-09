23a894264cef3696f3cb36552bdca27ef58d823a
Kris <kristof.solak@gmail.com>
2021-11-09 12:46:55 +0100

    first commit

---
+++ src/main/java/com/example/awsProba/AwsProbaApplication.java
@@ -0,0 +1,13 @@
+package com.example.awsProba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsProbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsProbaApplication.class, args);
+	}
+
+}
