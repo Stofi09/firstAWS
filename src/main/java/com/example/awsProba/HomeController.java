094dad44ed78454fc1ae0721f3572df70c9bb564
Kris <kristof.solak@gmail.com>
2021-11-09 14:55:48 +0100

    asd

--- src/main/java/com/example/awsProba/HomeController.java
+++ src/main/java/com/example/awsProba/HomeController.java
@@ -1,11 +1,13 @@
 package com.example.awsProba;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
 
+    @CrossOrigin
     @RequestMapping("/")
     public String index(){
         return "sikerult";
